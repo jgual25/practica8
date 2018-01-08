857d19e93c92e301e2e9a8dbeb83716e401c97f6
Your Name <you@example.com>
2018-01-08 13:52:47 +0100

    estils css

--- readgooglemaps.js
+++ readgooglemaps.js
@@ -17,7 +17,19 @@ function viewJSON() {
     loadJSON(function(response) {
         /* Aqui l'objecte response representa l'objecte JSON que ens 
            ha retornat el servidor */
-        document.getElementById("results").innerHTML = response.results[0].name;
+        var str = "<tr><th>Nom</th><th>Latitud</th><th>Longitud</th><th>Adreça</th><th>Tipus establiment</th><th>Icona</th></tr>";
+        for (var i=0; i<response.results.length; i++){
+
+        str += "<tr><td>"+ response.results[i].name + "</td>"+
+        "<td>"+ response.results[i].geometry.location.lat + "</td>"+
+        "<td>"+ response.results[i].geometry.location.lng + "</td>"+
+        "<td>"+ response.results[i].vicinity + "</td>"+
+        "<td>"+ response.results[i].types + "</td>"+
+        "<td><img src=' "+ response.results[i].icon+"'></td></tr>";
+        }
+        console.log(str);
+
+        document.getElementById("taula").innerHTML = str;
+        
     });
 }
-
